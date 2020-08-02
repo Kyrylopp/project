d2607489302623c3f32ce0fd0100880612f0abf1
Kirill <example@gmail.com>
2020-08-02 12:54:46 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let  number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+//alert("Hallo");
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//const answere = prompt("Вам есть 18?", "18");
+//console.log(typeof(answere));  
+
+const answere = [];
+
+answere[0] = prompt('Как ваше имя?', "");
+answere[1] = prompt('Как ваша фамилия?', "");
+answere[2] = prompt('Сколько вам лет?', "");
+
+document.write(answere);  
